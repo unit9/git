e3eaa08878fbccd6f7381924b33a86826ff32f45
Maciej Zasada <maciejzsd@gmail.com>
2014-08-07 18:27:12 +0200

    working recursive modules execution

--- .system/models/module.py
+++ .system/models/module.py
@@ -2,7 +2,83 @@ import os, sys
 
 class Module:
 
+  modules_by_name = {}
+  modules_by_full_name = {}
+
   @staticmethod
-  def get():
-    module_yaml_path = os.path.join(sys.argv[1], 'module.yaml')
-    print 'module yaml path: %s' % module_yaml_path
+  def get_by_name(name):
+    '''
+    Returns an array of modules of a given name.
+    Null if no module with a given name exists.
+    '''
+    if not name:
+      return None
+    if name in Module.modules_by_name:
+      return Module.modules_by_name[name]
+    elif name in Module.modules_by_full_name:
+      return [Module.modules_by_full_name[name]]
+    return None
+
+  def __init__(self, name, parent_full_name, path, submodules):
+    self.name = name
+    self.full_name = name
+    if parent_full_name:
+        self.full_name = parent_full_name + '/' + name
+    self.path = path
+    self.submodules = []
+    self.add_submodules(submodules)
+    self.register()
+    return
+
+  def register(self):
+    if not self.name in Module.modules_by_name:
+      Module.modules_by_name[self.name] = []
+    if self.full_name in Module.modules_by_full_name:
+      raise Exception('Duplicate module full name %s' % self.full_name)
+    Module.modules_by_name[self.name].append(self)
+    Module.modules_by_full_name[self.full_name] = self
+    return
+
+  def add_submodules(self, submodules):
+    if not submodules:
+        return
+    for submodule in submodules:
+      sub_submodules = None
+      if 'modules' in submodule:
+        sub_submodules = submodule['modules']
+      self.submodules.append(Module(submodule['name'], self.full_name, self.path + '/' + submodule['path'], sub_submodules))
+    pass
+
+  def perform_action(self, action, args):
+    if hasattr(self, action):
+      getattr(self, action)(args)
+    else:
+     raise Exception('unknown action \'%s\'' % action)
+    for module in self.submodules:
+      module.perform_action(action, args)
+    pass
+
+  def bootstrap(self, args):
+    pass
+
+  def develop(self, args):
+    pass
+
+  def test(self, args):
+    pass
+
+  def run(self, args):
+    self.run_script('run', args);
+    pass
+
+  def deploy(self, args):
+    pass
+
+  def get_path_to_script(self, script):
+    return os.path.join(self.path, script)
+
+  def run_script(self, script_name, args):
+    script_path = self.get_path_to_script(script_name)
+    if os.path.isfile(script_path):
+      os.system(script_path + ' %s' % (' '.join(args)))
+    return
--- .system/models/project.py
+++ .system/models/project.py
@@ -1,17 +1,24 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../libs'))
 
-from system import System
 import yaml
+from system import System
+from models.module import Module
 
 class Project:
+
+  root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+
+  @staticmethod
+  def get_root_path():
+    return Project.root_path
 
   @staticmethod
   def get():
     '''
     Returns a Project instance resolving the file path automatically.
     '''
-    project_yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../project.yaml'))
+    project_yaml_path = os.path.join(Project.get_root_path(), 'project.yaml')
     return Project.from_file(project_yaml_path)
 
   @staticmethod
@@ -27,6 +34,8 @@ class Project:
     '''
     Constructor.
     '''
+    self.modules = []
+    self.modules_by_name = {}
     return
 
   def read_yaml(self, path):
@@ -41,5 +50,25 @@ class Project:
       raise Exception(('Your project.yaml api_version: %d does not match the current infrastructure\'s API version: %d. '
         'Please consult your sysadmin or documentation in order to upgrade your config file.') % (data['api_version'], System.API_VERSION))
     # Apply the data.
-    print 'ok'
+    for module in data['modules']:
+      self.add_module(module)
+    return
+
+  def add_module(self, module):
+    module_instance = Module(module['name'], None, os.path.join(Project.root_path, module['path']), module['modules'])
+    self.modules.append(module_instance)
+    self.modules_by_name[module['name']] = module_instance
+    return
+
+  def perform_action(self, action, module_name, module_args):
+    modules = None
+    if module_name:
+      modules = Module.get_by_name(module_name)
+      if not modules:
+        print 'Could not find any module named \'%s\'' % module_name
+        return
+    else:
+      modules = self.modules
+    for module in modules:
+      module.perform_action(action, module_args)
     return
